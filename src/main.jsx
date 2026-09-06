330ac07dd692b9ee38660d98ce63374e776a252b
Eizer Relayson <egrelayson1@alum.up.edu.ph>
2026-09-06 02:57:55 +0000

    Set up dummy Paypal checkout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const initialOptions = {
+  clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
+  currency: 'USD',
+  intent: 'capture',
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PayPalScriptProvider options={initialOptions}>
+      <App />
+    </PayPalScriptProvider>
   </StrictMode>,
 )
